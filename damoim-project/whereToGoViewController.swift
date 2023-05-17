1c12a93d32cfa3022ba20b515a774b100f5e5335
cyujin <chldbwls115@naver.com>
2023-05-17 17:26:48 +0900

    끝

--- damoim-project/whereToGoViewController.swift
+++ damoim-project/whereToGoViewController.swift
@@ -541,85 +541,85 @@ class whereToGoViewController: UIViewController, NMFLocationManagerDelegate, CLL
 //    }
     
     // 히트맵 표시를 검출수 설정 값 이상만 표시하게 수정
-    func createHeatmap(with locations: [Location]) {
-        circleOverlays.forEach { overlay in
-            overlay.mapView = nil
-        }
-        circleOverlays.removeAll()
+        func createHeatmap(with locations: [Location]) {
+            circleOverlays.forEach { overlay in
+                overlay.mapView = nil
+            }
+            circleOverlays.removeAll()
 
-        // UserDefaults에서 minCount 값 가져오기
-        let minCount = UserDefaults.standard.integer(forKey: "minCount") == 0 ? 3 : UserDefaults.standard.integer(forKey: "minCount")
+            // UserDefaults에서 minCount 값 가져오기
+            let minCount = UserDefaults.standard.integer(forKey: "minCount") == 0 ? 3 : UserDefaults.standard.integer(forKey: "minCount")
 
-        for location in locations {
-            if location.count_catch >= minCount { // count_catch 값이 minCount 이상인 경우에만 히트맵 생성
-                let circleOverlay = NMFCircleOverlay(NMGLatLng(lat: location.latitude, lng: location.longitude), radius: calculateRadius(from: location.count_catch))
-                circleOverlay.fillColor = calculateColor(from: location.count_catch)
-                circleOverlay.mapView = naverMapView.mapView
-                circleOverlays.append(circleOverlay)
+            for location in locations {
+                if location.count_catch >= minCount { // count_catch 값이 minCount 이상인 경우에만 히트맵 생성
+                    let circleOverlay = NMFCircleOverlay(NMGLatLng(lat: location.latitude, lng: location.longitude), radius: calculateRadius(from: location.count_catch))
+                    circleOverlay.fillColor = calculateColor(from: location.count_catch)
+                    circleOverlay.mapView = naverMapView.mapView
+                    circleOverlays.append(circleOverlay)
 
-                // 레이블 생성
-                let label = UILabel()
-                label.text = "\(location.count_catch)"
-                label.textAlignment = .center
-                label.textColor = .black
-                label.font = UIFont.systemFont(ofSize: 25)
-                label.frame = CGRect(x: 0, y: 0, width: circleOverlay.radius * 2, height: circleOverlay.radius)
-                label.layer.cornerRadius = circleOverlay.radius
-                label.layer.masksToBounds = true
-                label.isUserInteractionEnabled = false
+                    // 레이블 생성
+                    let label = UILabel()
+                    label.text = "\(location.count_catch)"
+                    label.textAlignment = .center
+                    label.textColor = .black
+                    label.font = UIFont.systemFont(ofSize: 25)
+                    label.frame = CGRect(x: 0, y: 0, width: circleOverlay.radius * 2, height: circleOverlay.radius)
+                    label.layer.cornerRadius = circleOverlay.radius
+                    label.layer.masksToBounds = true
+                    label.isUserInteractionEnabled = false
 
-                // 지도 위에 레이블 추가
-                if let labelTextImage = labelToImage(label) {
-                    let labelMarker = NMFMarker(position: NMGLatLng(lat: location.latitude, lng: location.longitude))
-                    labelMarker.iconImage = NMFOverlayImage(image: labelTextImage)
-                    labelMarker.iconTintColor = .clear
-                    labelMarker.mapView = naverMapView.mapView
-                    circleLabels.append(labelMarker)
+                    // 지도 위에 레이블 추가
+                    if let labelTextImage = labelToImage(label) {
+                        let labelMarker = NMFMarker(position: NMGLatLng(lat: location.latitude, lng: location.longitude))
+                        labelMarker.iconImage = NMFOverlayImage(image: labelTextImage)
+                        labelMarker.iconTintColor = .clear
+                        labelMarker.mapView = naverMapView.mapView
+                        circleLabels.append(labelMarker)
+                    }
                 }
             }
         }
-    }
 
-    
-    // minCount = 3 으로 되어있음
-    func calculateRadius(from count_cleanup: Int) -> Double {
-           let smallRadius = 30.0
-           let mediumRadius = 70.0
-           let largeRadius = 100.0
+        
+        // minCount = 3 으로 되어있음
+        func calculateRadius(from count_cleanup: Int) -> Double {
+               let smallRadius = 30.0
+               let mediumRadius = 70.0
+               let largeRadius = 100.0
 
-           if count_cleanup <= 5 {
-               return smallRadius
-           } else if count_cleanup > 5 && count_cleanup <= 10 {
-               return mediumRadius
-           } else {
-               return largeRadius
-           }
-    }
+               if count_cleanup <= 5 {
+                   return smallRadius
+               } else if count_cleanup > 5 && count_cleanup <= 10 {
+                   return mediumRadius
+               } else {
+                   return largeRadius
+               }
+        }
 
-    
-    // 앱 시작후 히트맵 색깔 수정, 검출 수 조정과 관계없이 히트맵이 검출 수에 의해 색으로 구분되어 표시됌
-    func calculateColor(from count_cleanup: Int) -> UIColor {
-        let color1 = UIColor.green
-        let color2 = UIColor.systemYellow
-        let color3 = UIColor.red
-//        let progress = CGFloat(count_cleanup)/10.0
-//        let color = UIColor.interpolate(from: color1, to: color2, progress: progress)
-//
-//        // count 값에 따라 원하는 색상 값을 반환합니다.
-//        if count_cleanup >= minCount {
-//            return UIColor.red.withAlphaComponent(0.5)
-//        } else {
-//            return UIColor.blue.withAlphaComponent(0.5)
-//        }
-        if count_cleanup <= 5 {                 // 검출 수가 5이하일 때, 히트맵 파란색
-                return color1.withAlphaComponent(0.5)
-            } else if count_cleanup > 5 && count_cleanup <= 10 {        // 검출 수 6이상 10이하이면 히트맵 주황색
-                return color2.withAlphaComponent(0.5)
-            } else {                                        // 그 이상 검출되면, 빨간색으로 표시
-                return color3.withAlphaComponent(0.5)
-            }
+        
+        // 앱 시작후 히트맵 색깔 수정, 검출 수 조정과 관계없이 히트맵이 검출 수에 의해 색으로 구분되어 표시됌
+        func calculateColor(from count_cleanup: Int) -> UIColor {
+            let color1 = UIColor.green
+            let color2 = UIColor.systemYellow
+            let color3 = UIColor.red
+    //        let progress = CGFloat(count_cleanup)/10.0
+    //        let color = UIColor.interpolate(from: color1, to: color2, progress: progress)
+    //
+    //        // count 값에 따라 원하는 색상 값을 반환합니다.
+    //        if count_cleanup >= minCount {
+    //            return UIColor.red.withAlphaComponent(0.5)
+    //        } else {
+    //            return UIColor.blue.withAlphaComponent(0.5)
+    //        }
+            if count_cleanup <= 5 {                 // 검출 수가 5이하일 때, 히트맵 파란색
+                    return color1.withAlphaComponent(0.5)
+                } else if count_cleanup > 5 && count_cleanup <= 10 {        // 검출 수 6이상 10이하이면 히트맵 주황색
+                    return color2.withAlphaComponent(0.5)
+                } else {                                        // 그 이상 검출되면, 빨간색으로 표시
+                    return color3.withAlphaComponent(0.5)
+                }
 
-    }
+        }
     
 
     //구조체 정의
